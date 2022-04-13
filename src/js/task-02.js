2f9ad8f4279e4e0fc2a49506113515ad132c315d
MariaStreltsova <Mariastreltova@gmail.com>
2022-04-13 22:55:44 +0300

    Completes task 02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listRef = document.querySelector('#ingredients')
+
+ingredients.map(ingredient => {
+  const itemRef = document.createElement("li");
+  itemRef.textContent = ingredient;
+  itemRef.classList.add("item")
+  listRef.append(itemRef)
+  console.log(itemRef)
+})
