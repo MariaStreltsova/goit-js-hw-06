2de90323fc7acec45cf6fda0fa9032d04b446a79
MariaStreltsova <Mariastreltova@gmail.com>
2022-04-17 13:29:44 +0300

    update task 05

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -4,8 +4,9 @@ const refs = {
 }
 refs.input.addEventListener('input', onInputChange)
 function onInputChange(event) {
-    refs.spanText.textContent = event.currentTarget.value;
     if (event.currentTarget.value === "") {
-       return refs.spanText.textContent = "Anonymous!"
+    refs.spanText.textContent = "Anonymous"
+    } else {
+        refs.spanText.textContent = event.currentTarget.value;
     }
 }
