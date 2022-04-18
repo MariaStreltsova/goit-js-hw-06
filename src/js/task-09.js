aabe9382ffb0ccc7021f508a195f7c7fc5ae62eb
MariaStreltsova <Mariastreltova@gmail.com>
2022-04-18 12:30:37 +0300

    update 09 task

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,13 +1,12 @@
 const btnEl = document.querySelector('.change-color')
 const textColorEl = document.querySelector('.color')
-const body = document.querySelector('body')
-console.dir(textColorEl)
+
 btnEl.addEventListener('click', onChangeColor)
 function onChangeColor() {
   const newColor = getRandomHexColor();
-  console.log("background:", document.body.style.backgroundColor = newColor);
+  document.body.style.backgroundColor = newColor;
   textColorEl.textContent = newColor;
-  console.log("text:", textColorEl.textContent)
+
 
 }
 
