bbcf155798970b9f60584065f05648f19f878a0f
MariaStreltsova <Mariastreltova@gmail.com>
2022-04-22 10:52:09 +0300

    update 01

--- js/task-01.js
+++ js/task-01.js
@@ -2,8 +2,8 @@ const navRef = document.querySelectorAll('.item')
 console.log('Number of categories:', navRef.length);
 
 navRef.forEach(function (itemName) {
-    const categoriesRef = itemName.querySelector('h2')
-    const elementsLengthRef = itemName.querySelector('ul')
+    const categoriesRef = itemName.firstElementChild;
+    const elementsLengthRef = itemName.lastElementChild;
     console.log('Category :', categoriesRef.textContent)
     console.log('Elelments :', elementsLengthRef.children.length)
 })
